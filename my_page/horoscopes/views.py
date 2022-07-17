d1f6b4976ecfabd688fcd112e3c779e49b71fb7e
name <Romanzaitsev25@yandex.by>
2022-07-17 18:18:34 +0300

    new information

--- my_page/horoscopes/views.py
+++ my_page/horoscopes/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, \
     HttpResponseRedirect
-from django.template.loader import render_to_string
+from django.shortcuts import render
 from django.urls import reverse
+from dataclasses import dataclass
 
 # Create your views here.
 zodiac_dict = {
@@ -24,22 +24,25 @@ zodiac_dict = {
 
 def index(request):
     zodiacs = list(zodiac_dict)
-    li_elements = ''
-    for sign in zodiacs:
-        redirect_path = reverse('horoscope-name', args=[sign, ])
-        li_elements += f"<li><a href='{redirect_path}'>{sign.title()}</a></li>"
-    response = f'''
-    <ol>
-        {li_elements}
-    </ol>
-    '''
-    return HttpResponse(response)
+    context = {
+        'zodiacs': zodiacs,
+    }
+    return render(request, 'horoscopes/index.html', context=context)
+    # li_elements = ''
+    # for sign in zodiacs:
+    #     redirect_path = reverse('horoscope-name', args=[sign, ])
+    #     li_elements += f"<li><a href='{redirect_path}'>{sign.title()}</a></li>"
+    # response = f'''
+    # <ol>
+    #     {li_elements}
+    # </ol>
+    # '''
+    # return HttpResponse(response)
     # response = '<br>'.join(zodiacs)
 
 
 # def get_info_about_sign_zodiac(request, sign_zodiac: str):
 #     description = zodiac_dict.get(sign_zodiac, None)
-#     # response = render_to_string('horoscope/info_zodiac.html')
 #     if description:
 #         return HttpResponse(f'<h2>{description}</h2>')
 #     # if sign_zodiac == 'leo':
@@ -54,10 +57,23 @@ def index(request):
 #     else:
 #         return HttpResponseNotFound(f'Неизвестный знак зодиака-{sign_zodiac}')
 
+
 def get_info_about_sign_zodiac(request, sign_zodiac: str):
     # response = render_to_string('horoscopes/info_zodiac.html')
     # return HttpResponse(response)
-    return render(request, 'horoscopes/info_zodiac.html')
+    description = zodiac_dict.get(sign_zodiac)
+    data = {
+        'description_zodiacs': description,
+        'sign': sign_zodiac,
+        # 'mu_int': 100,
+        # 'my_float': 2.5,
+        # 'my_list': [],
+        # 'my_tuple': (1, 333, 5,),
+        # 'my_dict': {'name': 'Roman', 'age': 30},
+        # 'my_class': Person('Roman', 25),
+        # 'value': 0,
+    }
+    return render(request, 'horoscopes/info_zodiac.html', context=data)
 
 
 def get_info_about_sign_zodiac_by_number(request, sign_zodiac: int):
--- my_page/my_page/settings.py
+++ my_page/my_page/settings.py
@@ -58,7 +58,9 @@ TEMPLATES = [
     # путь до папки шаблонов. BASE_DIR/'horoscope'/'templates'
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
